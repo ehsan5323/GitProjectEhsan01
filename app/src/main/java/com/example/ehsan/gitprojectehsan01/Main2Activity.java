6aed2f542f1ca7712c0a3647e75c6f378361d08c
Ehsan Ahmadi <ehsan5323@gmail.com>
2017-11-18 08:07:26 +0330

    commit change background textview

--- app/src/main/java/com/example/ehsan/gitprojectehsan01/Main2Activity.java
+++ app/src/main/java/com/example/ehsan/gitprojectehsan01/Main2Activity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+
 public class Main2Activity extends AppCompatActivity {
     TextView textView;
     @Override
@@ -12,5 +13,6 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         textView= (TextView) findViewById(R.id.act2_txt);
         textView.setText("code ok");
+        textView.setTextColor(getResources().getColor(R.color.colorAccent));
     }
 }
