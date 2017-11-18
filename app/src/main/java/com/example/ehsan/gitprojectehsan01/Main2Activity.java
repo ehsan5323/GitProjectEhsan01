ef23a4903fd705c36de452a4d75f59a8fa42cace
Ehsan Ahmadi <ehsan5323@gmail.com>
2017-11-18 07:45:10 +0330

    change settext in act2

--- app/src/main/java/com/example/ehsan/gitprojectehsan01/Main2Activity.java
+++ app/src/main/java/com/example/ehsan/gitprojectehsan01/Main2Activity.java
@@ -2,12 +2,15 @@ package com.example.ehsan.gitprojectehsan01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        textView= (TextView) findViewById(R.id.act2_txt);
+        textView.setText("code ok");
     }
 }
